4a0c87f330f988b66cbc20bb9bdcab9cfcef5b5e
KANHAIYA JI <kanhaiyaji14@gmail.com>
2025-12-05 20:31:09 +0530

    connect db

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 import express from 'express';
 import bodyParser from 'body-parser'; // Allows us to get body of incoming post request
-import userRoutes from './routes/users.js' //userRoutes is just a keyword, it will recieve whatever you exported as default from users.js
+import userRoutes from './routes/users.js'; //userRoutes is just a keyword, it will recieve whatever you exported as default from users.js
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'; 
+
+dotenv.config();
 
 const app = express(); // created an app using express object
-const PORT = 5000; // port for the app
+const PORT = process.env.port | 5000; // port for the app
 app.use(bodyParser.json()); // Specified that json data will be used in application
 
 // Tells express whenever someone hits /users route, use userRoutes to handle those requests 
@@ -14,13 +18,23 @@ app.use('/users', userRoutes);
 // app.get() function accepts two parameters, first one is path, and second is callback, where we define 
 // what happens when get request is called.
 // callback function also has two parameters. request(incoming request) and result(the information we want to send)
-app.get('/', (req, res) => {
-    console.log('[GET ROUTE]');
-    res.send("HELLO FROM NODE");
-})
+// app.get('/', (req, res) => {
+//     console.log('[GET ROUTE]');
+//     res.send("HELLO FROM NODE");
+// })
 
-// listen method to make app listen for incoming requests. 
-//It takes two argumetns, 
-// 1. Port - here we would be listening incoming requests from client side
-// 2. Callback function that would be triggered only once when our server starts.
-app.listen(PORT, () => console.log(`Server running on port: http//localhost:${PORT}`));
+//DataBase Connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Conncted to Mongo Db");
+
+        // listen method to make app listen for incoming requests. 
+        //It takes two argumetns, 
+        // 1. Port - here we would be listening incoming requests from client side
+        // 2. Callback function that would be triggered only once when our server starts.
+        // app.listen(PORT, () => console.log(`Server running on port: http//localhost:${PORT}`));
+        app.listen(PORT, () => console.log(`Server running on port: http//localhost:${PORT}`));
+    })
+    .catch((err) => {
+        console.error('MongoDB Connection Error', err);
+    })
--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,45 @@
 import express from 'express';
 const router = express.Router() // Creates Fresh router instance
 import { v4 as uuidv4 } from 'uuid';
-
-let users = [
-    {
-        firstName: 'john',
-        lastName: 'Doe',
-        email: 'johndoe@test.com'
-    },
-    {
-        firstName: 'Alice',
-        lastName: 'Smith',
-        email: 'alicesmith@test.com'
-    },
-]
+import user from '../models/users.js'
 
 //sets the get route to respond to http get requests
-router.get('/', (req, res) => {
-    res.send(users);
+router.get('/', async(req, res) => {
+    const users = await user.find()
+    res.json(users);
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
     const { id } = req.params;
-    const foundUser = users.find( (user) => user.id === id);
-    res.send(foundUser)
+    const foundUser = await user.findById(id);
+    res.json(foundUser)
 })
 
 //sets the post route
-router.post('/', (req, res) => {
-    const user = req.body;
-    users.push( { ...user, id: uuidv4() } );
-    res.send(`${user.firstName} has been added to the database`)
+router.post('/', async(req, res) => {
+    try {
+        const { firstName, lastName, email} = req.body;
+        const newUser = await user.create( { firstName, lastName, email});
+        res.status(201).json(newUser);
+    } catch(err) {
+        res.status(400).json({message: 'Could not create user', error: err.message})
+    }
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
     const { id } = req.params;
-    users = users.filter((user) => user.id !== id) //Soft Delete
-    res.send(`${id} deleted successfully from database`)
+    const deleteUser = await user.findByIdAndDelete(id)
+    res.json(`${id} deleted successfully from database`)
 })
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', async(req, res) => {
     const { id } = req.params;
-    const { firstName, lastName, email } = req.body;
-    const user = users.find( user => user.id === id);
-    if (firstName) user.firstName = firstName
-    if (lastName) user.lastName = lastName
-    if (email) user.email = email
-    res.send(`User with the ${id} has been updated`)
+    const updates = req.body
+    const updateUser = await user.findByIdAndUpdate(id, updates, { new: true, runValidators: true});
+
+    if (!updateUser) return res.status(404).json({message: 'User not found'})
+
+    res.json(updateUser);
 })
 
 //Make this router available to be imported in other files
